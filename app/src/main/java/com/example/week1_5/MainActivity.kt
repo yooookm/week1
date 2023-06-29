ebf5ef5f8529b9003dd7dcda836cfcb70ff38755
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2023-06-29 16:40:41 +0900

    first

---
+++ app/src/main/java/com/example/week1_5/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.week1_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun BtnClick(v: View){
+        setContentView(R.layout.contact_view)
+    }
+
+}
