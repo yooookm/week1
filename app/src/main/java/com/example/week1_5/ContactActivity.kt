a62cee00f3cef95044c5418d6f8bc73e3cb2db0d
ckalsgh56 <ckalsgh56@naver.com>
2023-06-30 11:42:58 +0900

    adapter 하다 말음

--- app/src/main/java/com/example/week1_5/ContactActivity.kt
+++ app/src/main/java/com/example/week1_5/ContactActivity.kt
@@ -8,6 +8,10 @@ class ContactActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contact_view)
-    }
 
+        val contactAdapter = MainListAdapter(this, contactList)
+        mainListView.adapter = contactAdapter
+    }
 }
+
+var contactList = arrayListOf<contactInfo>()
---
+++ app/src/main/java/com/example/week1_5/contactInfo.kt
@@ -0,0 +1,7 @@
+package com.example.week1_5
+
+class contactInfo (name: String, contactNum: String, photo: String){
+        var name: String = name
+        var contactNum = contactNum
+        var photo = photo
+}
