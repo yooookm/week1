42c25f4bc855976ac0f2d249de39ad27080a95dc
ckalsgh56 <ckalsgh56@naver.com>
2023-07-02 18:22:21 +0900

    갤러리 이미지 띄우기 성공

---
+++ app/src/main/java/com/example/week1_5/imageInfo.kt
@@ -0,0 +1,11 @@
+package com.example.week1_5
+
+import android.net.Uri
+import java.util.Date
+
+class imageInfo (id: Long, name: String, uri: Uri, date: Date){
+    val id = id
+    val name = name
+    val uri = uri
+    val date = date
+}
