5733265e1bca96615f57a1381d10be8a110b701d
ckalsgh56 <ckalsgh56@naver.com>
2023-07-03 20:09:42 +0900

    gallery swipe

--- app/src/main/java/com/example/week1_5/GalleryAdapter.kt
+++ app/src/main/java/com/example/week1_5/GalleryAdapter.kt
@@ -1,49 +1,43 @@
 package com.example.week1_5
 
-import android.app.AlertDialog
-import android.media.Image
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.util.Date
 
-class GalleryAdapter(val myImageList: List<imageInfo>) :
+class GalleryAdapter(private val myImageList: List<imageInfo>) :
     RecyclerView.Adapter<GalleryAdapter.MyViewHolder>() {
-    // onCreateViewHolder에서는 item 레이아웃을 inflate 합니다.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.gallery_item, parent, false)
         return MyViewHolder(v)
     }
 
-    // onBindViewHolder에서는 ViewHolder에 데이터를 바인딩 합니다.
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         Glide.with(holder.imageView.context).load(myImageList[position].uri).into(holder.imageView)
     }
 
-    // getItemCount에서는 아이템의 총 개수를 반환합니다.
     override fun getItemCount(): Int {
         return myImageList.size
     }
-    // ViewHolder 클래스
+
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imageView: ImageView = itemView.findViewById(R.id.image)
+        val imageView: ImageView = itemView.findViewById(R.id.image)
+
         init {
             itemView.setOnClickListener {
                 val position: Int = adapterPosition
                 val context = itemView.context
                 val image = myImageList[position]
+                val imageUriList = myImageList.map { it.uri } as ArrayList<Uri>
+                val imageNameList = myImageList.map { it.name } as ArrayList<String>
+                val imageDateList = myImageList.map { it.date } as ArrayList<Date>
 
-
-                context.startActivity(ImageViewActivity.newIntent(context, image.uri, image.name, image.date))
-
+                context.startActivity(ImageViewActivity.newIntent(context, position, imageUriList, imageNameList, imageDateList))
             }
         }
-
-
     }
-
-
 }
