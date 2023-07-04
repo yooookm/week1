9f6961d67fd73fd7f89e5b239ab893b28c74911b
ckalsgh56 <ckalsgh56@naver.com>
2023-07-05 04:22:49 +0900

    kakaotalk complete

--- app/src/main/java/com/example/week1_5/QuestionActivity.kt
+++ app/src/main/java/com/example/week1_5/QuestionActivity.kt
@@ -2,14 +2,16 @@ package com.example.week1_5
 
 import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class QuestionActivity: AppCompatActivity() {
+class QuestionActivity : AppCompatActivity() {
     private lateinit var imageUriList: List<Uri>
     private lateinit var questionList: List<String>
+    private lateinit var answerList: MutableList<String?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,23 @@ class QuestionActivity: AppCompatActivity() {
         imageUriList = intent.getParcelableArrayListExtra("imageUriList")!!
         questionList = intent.getStringArrayListExtra("keywordsList")!!
 
+        var answer: MutableList<String> = mutableListOf()
+
+
         val chatHistory = findViewById<RecyclerView>(R.id.chat_history)
+        val questionAdapter = QuestionAdapter(imageUriList, questionList, answer.toMutableList())
+        chatHistory.layoutManager = LinearLayoutManager(this)
+        chatHistory.adapter = questionAdapter
 
-        // rest of the code
+        val apiButton = findViewById<Button>(R.id.api_button)
+        val chatbox = findViewById<EditText>(R.id.chatbox)
+        apiButton.setOnClickListener {
+            val userinput = chatbox.text.toString()
+
+            questionAdapter.addAnswer(userinput)
+            questionAdapter.notifyDataSetChanged() // 버튼이 클릭될 때마다 RecyclerView 갱신
+
+            chatbox.setText("")
+        }
     }
-
 }
--- app/src/main/java/com/example/week1_5/QuestionAdapter.kt
+++ app/src/main/java/com/example/week1_5/QuestionAdapter.kt
@@ -1,11 +1,10 @@
 package com.example.week1_5
 
 import android.net.Uri
-import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -24,22 +23,36 @@ class QuestionAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentUri = imageUris[position]
-        Glide.with(holder.imageView.context).load(currentUri).into(holder.imageView)
+        Log.d("position", "${position}")
+        Log.d("answer size", "${answers.size}")
+        if (answers.size >= position) {
+            val currentUri = imageUris[position]
+            Glide.with(holder.imageView.context).load(currentUri).into(holder.imageView)
 
-        val currentQuestion = questions[position]
-        holder.questionTextView.text = currentQuestion
+            val currentQuestion = questions[position]
+            holder.questionTextView.text = currentQuestion
+        }
 
-        val currentAnswer = answers[position]
-        holder.answerTextView.text =
-            (currentAnswer ?: "") as Editable? // 답변이 null일 경우, 아무것도 표시하지 않습니다
+        if (answers.size > position) {  // answers.size와 position을 비교해야합니다.
+            val currentAnswer = answers[position]
+            holder.answerTextView.text = currentAnswer ?: "" // 답변을 설정하는 코드를 추가합니다.
+        } else {
+            holder.answerTextView.text = ""  // 답변이 null일 경우, 아무것도 표시하지 않습니다
+        }
     }
+
 
     override fun getItemCount() = questions.size
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.image)
         val questionTextView: TextView = itemView.findViewById(R.id.question)
-        val answerTextView: EditText = itemView.findViewById(R.id.answer)
+        val answerTextView: TextView = itemView.findViewById(R.id.answer)
+    }
+
+    fun addAnswer(userinput: String) {
+        Log.d("answer check","before${answers.size}")
+        answers.add(userinput)
+        Log.d("answer check","after${answers.size}")
     }
 }
